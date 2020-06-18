8339e4400524282b2a0dbd15c680875e1973928d
tkd708 <take708gym@gmail.com>
2020-06-19 00:39:52 +1000

    site url

--- src/website-config.ts
+++ src/website-config.ts
@@ -54,7 +54,7 @@ const config: WebsiteConfig = {
   coverImage: 'img/blog-index.jpg',
   logo: 'img/logo_initial2.png',
   lang: 'en',
-  siteUrl: 'https://naoya-takeda.netlify.com',
+  siteUrl: 'https://naoya-takeda.netlify.app',
   facebook: 'https://www.facebook.com/ghost',
   twitter: 'https://twitter.com/tryghost',
   showSubscribe: true,
